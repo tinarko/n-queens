1a2c2eb2c445834c0279060cee98ac36c7b608af
tinarko <tinarchan@gmail.com>
2017-02-16 12:11:08 -0800

    Initial attempt at findNRooksSolution

--- src/solvers.js
+++ src/solvers.js
@@ -16,80 +16,54 @@
 
 
 window.findNRooksSolution = function(n) {
-  var solution = undefined; 
-
   // create a new board
+  if (n === 0) {
+    return [[]];
+  }
   var board = new Board({n: n});
-  // create rook counter = 0
 
-  // ADD ROOKS THROUGH RECURSION??
-
-  // base case: counter = n
-
-  // continue if: counter < n 
-
-  // recursive function(counter, existing option)
-
-    // define index = find last index of existing option where value = 1
-
-    // for loop: start from index +1 || row = 0, col = 0 
-    // then loop through row first, then col
-      
-      // set board[rowIndex][colIndex] = 1 to add the rook
-
-      // if !hasAnyRowConflicts && !hasAnyColConflicts
-
-        // increment counter and exit for loop
-
-      // else delete rook and continue for loop
-
-    // recusive call 
+  // create a function called addRook(board, rookNum, rowIndex, colIndex)
+  var addRook = function (board, rookNum, rowIndex, colIndex) {
+    //base case: rookNum = n
+    if (rookNum === n) {
+      //solution = board
+      return board; 
+    }
+    // continue if: rookNum < n
+    if (rookNum < n) {
+      //toggle rook at location(rowIndex, colIndex)
+      board.togglePiece(rowIndex, colIndex);
+      // if valid board: !hasAnyRowConflicts && !hasAnyColConflicts
+      if (!board.hasAnyRooksConflicts()) {
+        // increment rookNum
+        rookNum += 1;        
+      // else
+      } else {
+        //toggle rook
+        board.togglePiece(rowIndex, colIndex);
+      } 
+      // find next available location / update row and col values  
+      //if colIndex = n-1 
+      if (colIndex === n - 1) {
+        //rowIndex +=1
+        rowIndex += 1;
+        //colIndex = 0
+        colIndex = 0;
+      //else colIndex += 1; 
+      } else {
+        colIndex += 1;
+      }
+    }    
+    // recusive call (board, rookNum, rowIndex, colIndex)
+    addRook(board, rookNum, rowIndex, colIndex);
+  };
 
   // call recursive function
+  addRook(board, 0, 0, 0);
 
 
-
-
-
-
-
-
-
-// ----
-
-  //Create an initial empty array of length 16 [0,0,0,0],[]
-
-  //create rook count variable
-  //create next available array index placeholder
-  //Add rook 1
-  //while rook count < n . . .
-  //(for loop i = next available array; i < 15; i++)
-
-    //change array[i] to 1 to place a rook
-    //update rook count variable
-    //create a new Board with the array and ask if it passes the test
-    //if the board passes the test, rook count +=1 move to next rook . . .
-    //if it doesn't pass the test, move the new rook to the next available indext
-
-
-
-  //
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
-  return solution;
+  console.log('Single solution for ' + n + ' rooks:', JSON.stringify(board));
+  // return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
